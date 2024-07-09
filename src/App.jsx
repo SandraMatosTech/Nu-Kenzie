81583b55a41ce57d587c1cfa500cdeb09b6c0fd5
SandraMatos9 <matos.sandrarodrigues@hotmail.com>
2024-07-09 20:16:19 -0300

    update correcoes

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,7 @@
 import React, { useState } from 'react'
-import './App.css'
-import { Register } from '../src/components/Register/index.jsx'
-import { ValorTotal } from '../src/components/Total/index.jsx'
-import { ListaCards } from '../src/components/ListCards/index.jsx'
+import "./App.css";
+import "./styles/globalStyles.css";
+import "./styles/reset.css";
 import LandingPage from './pages/LandingPage'
 import Dashboard from './pages/Dashboard'
 
